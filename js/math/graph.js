e4a1e5344415274553ec0b4568e0234d21d8fb6d
Niraj Paudel <www.nirajpaudel@gmail.com>
2024-01-10 18:52:57 +0545

    Add Points & Segment

---
+++ js/math/graph.js
@@ -0,0 +1,51 @@
+class Graph {
+    constructor(points = [], segments = []) {
+        this.points = points;
+        this.segments = segments;
+    }
+
+    addPoint(point) {
+        this.points.push(point)
+    }
+
+    containsPoint(point) {
+        return this.points.find((p) => p.equals(point));
+    }
+
+    tryAddPoint(point) {
+        if(!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+
+        return false;
+    }
+
+    addSegment(seg) {
+        debugger
+        this.segments.push(seg);
+    }
+
+    containsSegment(seg) {
+        return this.segments.find((s)=> s.equals(seg));
+    }
+
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    
+    }
+
+    draw(ctx) {
+        for (const seg of this.segments) {
+            seg.draw(ctx);
+        }
+
+        for (const point of this.points) {
+            point.draw(ctx);
+        }
+    }
+}
